be4257d22d5d048d4950a3738ec74cb846f7dbd1
KalliAnn <kalliann.hale@gmail.com>
2025-06-06 11:19:29 -0400

    developing data loading & standardization module :)

---
+++ dataMart/R/load-tree-data/load_tree_data.R
@@ -0,0 +1,13 @@
+"
+Kalli A. Hale
+[if you're a contributor, your name goes here :)]
+___
+
+June 2025
+___
+
+This module coordinates our package's main export 
+  function for loading data into our datamart.
+"
+
+pacman::p_load(tidyr, dplyr)
---
+++ dataMart/R/load-tree-data/quality_validation.R
@@ -0,0 +1,6 @@
+"
+This module houses helper functions for data 
+  validation.
+"
+
+pacman::p_load(tidyr, dplyr)
